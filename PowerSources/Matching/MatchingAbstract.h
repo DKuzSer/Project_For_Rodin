f0ecd0ff0c615f53fe9ce30ffd5f05d9cbf1f980
Michail Gilev <mishagilevfgh@e1.ru>
2022-05-15 18:21:57 +0300

    Ввел формулы для согласования

--- PowerSources/Matching/MatchingAbstract.h
+++ PowerSources/Matching/MatchingAbstract.h
@@ -2,12 +2,29 @@
 #define MATCHINGABSTRACT_H
 
 #include <QObject>
+#include "constans.h"
 
 class MatchingAbstract : public QObject
 {
     Q_OBJECT
 public:
     MatchingAbstract();
+
+    virtual void SetBaseValue(double , double , double, double, double ){};
+    virtual void Calculate(){};
+    virtual void ViewFilters(int){};
+
+    double Ek;
+    double P;
+    int F;
+    double Rh;    //сопротивление нагрузки
+    double Q;
+
+    double Rin = 0.0;     // сопротивление на выходе транхистора
+    double C1 = 0.0;     // индуктивность
+    double L1 = 0.0;     // ёмкость
+    double C2 = 0.0;     // индуктивность
+    double L2 = 0.0;
 };
 
 #endif // MATCHINGABSTRACT_H
--- PowerSources/Matching/MatchingOneCircuit.h
+++ PowerSources/Matching/MatchingOneCircuit.h
@@ -2,11 +2,18 @@
 #define MATCHINGONECIRCUIT_H
 
 #include "MatchingAbstract.h"
+#include <math.h>
 
 class MatchingOneCircuit : public MatchingAbstract
 {
 public:
     MatchingOneCircuit();
+
+    void SetBaseValue(double , double , double, double, double );
+    void Calculate();
+    void ViewFilters(int number);
+
+    int flagFilters = 0; //0 - П-фильтра    1 - Модифицированный П-фильтр
 };
 
 #endif // MATCHINGONECIRCUIT_H
